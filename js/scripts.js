ada919ebb688e7d43c739c7540003a400520ff56
Jessie <jess.bell@me.com>
2017-11-17 15:11:04 -0800

    Add tests for returning a person's age on Venus, Mars and Jupter. Add tests for estimating the number of years a person has left to live on Earth. Add functionality to convert the remaining Earth years to years on Mercury, Venus, Mars, and Jupiter

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
+
 export class Age {
 
-  constructor(age) {
-    this.age = age;
-  }
-  ageCalculator() {
-    let ageinSeconds = this.age*365*86400;
-    return ageinSeconds;
-  }
-
-  mercuryCalculator(){
-    let mercuryAge =(this.age/ 0.24);
-    console.log(mercuryAge);
-    return mercuryAge;
-  }
-
-}
-
-export class AgeSeconds {
-
-  constructor (birthYear, birthMonth, birthDay, todayYear, todayMonth, todayDay, age) {
+  constructor (birthYear, birthMonth, birthDay, todayYear, todayMonth, todayDay) {
     this.birthYear = birthYear;
     this.birthMonth = birthMonth;
     this.birthDay = birthDay;
     this.todayYear = todayYear;
     this.todayMonth = todayMonth;
     this.todayDay = todayDay;
+    this.age = 0;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+    this.lifeLeft = 0;
+    this.mercuryLife = 0;
+    this.venusLife = 0;
+    this.marsLife = 0;
+    this.jupiterLife = 0;
+    this.earthExpectancy = 0;
+    this.mercuryExpectancy = 0;
+    this.venusExpectancy = 0;
+    this.marsExpectancy = 0;
+    this.jupiterExpectancy = 0;
   }
 
   currentAgeCalculator() {
 
     let years = this.todayYear - this.birthYear;
     let month = 0;
+    const secondCalculator = (60*60*24*365);
 
     if (this.birthMonth <= this.todayMonth) {
       month = this.todayMonth - this.birthMonth;
@@ -39,10 +38,31 @@ export class AgeSeconds {
     }
     let days = this.birthDay + this.todayDay;
 
-    let yearsSeconds = (years*365*86400);
+    let yearsSeconds = (years*secondCalculator);
     let monthSeconds =(month*31*86400);
     let daySeconds = (days*86400);
 
-    return yearsSeconds + monthSeconds + daySeconds;
+    this.age = yearsSeconds + monthSeconds + daySeconds;
+
+    this.mercuryAge = ((this.age / .24)/secondCalculator).toFixed(2);
+    this.venusAge = ((this.age / .62)/secondCalculator).toFixed(2);
+    this.marsAge = ((this.age / 1.88)/secondCalculator).toFixed(2);
+    this.jupiterAge = ((this.age / 11.86)/secondCalculator).toFixed(2);
+
+    let lifeLeft = Math.floor(Math.random() * 50);
+    this.lifeLeft = lifeLeft;
+
+    this.mercuryLife = lifeLeft / .24;
+    this.venusLife = lifeLeft / .62;
+    this.marsLife = lifeLeft / 1.88;
+    this.jupiterLife = lifeLeft / 11.86;
+
+    this.earthLife = (this.age/secondCalculator)+ lifeLeft;
+    this.mercuryExpectancy = this.mercuryAge + this.mercuryLife;
+    this.venusExpectancy = this.venusAge + this.venusLife;
+    this.marsExpectancy = this.marsLife + this.marsLife;
+    this.jupiterExpectancy = this.jupiterAge + this.jupiterLife;
+
   }
+
 }
--- spec/scripts-spec.js
+++ spec/scripts-spec.js
@@ -4,19 +4,49 @@ import { Age, AgeSeconds } from './../js/scripts.js';
 describe('Age', function() {
 
   it ('should convert a persons age into seconds', function() {
-    let age = new Age (29);
-    expect(age.ageCalculator()).toEqual(914544000);
+    let age = new Age (1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    expect(age.age).toEqual(938217600);
   })
 
   it('should convert the time between two dates into seconds', function() {
-    let age = new AgeSeconds(1988, 3, 9, 2017, 11, 17);
-    expect(age.currentAgeCalculator()).toEqual(938217600);
+    let age = new Age(1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    expect(age.age).toEqual(938217600);
   });
 
-  it ('should convert a persons Earth age to their Mars Age', function() {
-    let age = new Age (29);
-    expect(age.mercuryCalculator()).toEqual(6.96);
-  })
+  it ('should convert a persons Earth age to their Mercury age', function() {
+    let age = new Age (1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    expect(age.mercuryAge).toEqual('123.96');
+  });
+
+  it ('should convert a persons Earth age to their Venus age', function() {
+    let age = new Age (1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    expect(age.venusAge).toEqual('47.98');
+  });
+
+  it ('should convert a persons Earth age to their Mars age', function() {
+    let age = new Age (1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    expect(age.marsAge).toEqual('15.82');
+  });
+
+  it ('should convert a persons Earth age to their Jupiter age', function() {
+    let age = new Age (1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    console.log(age.jupiterAge);
+    expect(age.jupiterAge).toEqual('2.51');
+  });
+
+  it('should estimate the remaining number of years the user has left to live on Earth', function() {
+    let age = new Age (1988, 3, 9, 2017, 11, 17);
+    age.currentAgeCalculator();
+    expect(age.lifeLeft).toBeLessThan(50);
+  });
+
+
 
 
 });
