13bfa2ae83de3d173bda5357a4a6faeddee95dda
Jessie <jess.bell@me.com>
2017-11-19 13:22:52 -0800

    Change ouput text and add image placeholders for planets

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+Super Galactic Age Calculator
+
+This application was made to satisfy the week one code review for Epicodus Javascript course. The application is able to convert a person's age on earth to a person's age if they were on another planet in our galaxy. The application is also able to "predict" the user's life expectancy, and tell the user until how old the user will live on each planet.
+
+The application displays a form that accepts input from the user. The application asks the user to enter their age and the current date. When the user presses the submit button after the form, the application starts a series of calculations. First, the application converts the user's age into seconds. The application then converts the users' age in seconds to the user's age on each planet in years.
+
+The application also has the capability to "magically" predict how long the user will live. The application does this by generating a random number between zero and fifty, and then adds the random number to the user's current age. This is obviously by no means scientific, and the user can re-submit the form multiple times to get multiple answers for how long they will live.
 
 The Application shall:
   - take a person's age in years and convert it into seconds.
@@ -9,3 +16,28 @@ The Application shall:
   - return the age of a human in Jupiter years.
   - determine how many years a user has left to live on each planet.
       Because I used a random number generator, i'm not able to determine how many years exactly should be expected with each output. However, I did test that the random number generated is less than 50 (a number I just came up with.)
+
+Technologies used:
+
+  This application requires a web browser in order to run. I used several applications while developing this project. Those include:
+    -HTML / CSS
+    - Javascript / JQuery
+    - Gulp / Browserify / Vinyl-Source-Stream / Uglify / Bower
+    - Jshint
+    - Jasmine / Karma
+    - Babelify
+
+Set-Up:
+
+  In order to set-up this application, follow these directions:
+    - Clone the application to your desktop from Github
+    - Ensure Node Project Manager (NPM) is installed on your device. Initialize NPM in the project by entering "NPM init" at the root level of the directory.
+    - Ensure Bower is installed on your device. Initialize Bower in the project by entering "bower init" at the root level of the directory.
+
+GH-Pages Link:
+
+  Access this project through the following link:
+
+Known Bugs:
+
+  No known bugs at this time. 
--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -9,25 +9,37 @@ $(document).ready(function() {
 
     let birthMonth = $("#dob-month").val();
     birthMonth = parseInt(birthMonth);
-  
+
     let birthDay = $("#dob-day").val();
     birthDay = parseInt(birthDay);
-  
+
     let todayYear = $("#current-year").val();
     todayYear = parseInt(todayYear);
 
     let todayMonth = $("#current-month").val();
     todayMonth = parseInt(todayMonth);
-  
+
     let todayDay = $("#current-day").val();
     todayDay = parseInt(todayDay);
-  
+
     let userAge = new Age(birthYear, birthMonth, birthDay, todayYear, todayMonth, todayDay);
     userAge.currentAgeCalculator();
     // age.parseInt();
 
     // $("#output").show();
-    $(".earth-stats").text("On earth, you are" + userAge.earthLife + " Based on our galactic calculations, you will live " + userAge.lifeLeft + " years left, which means you will live until you are " + userAge.earthExpectancy + ".");
+    $(".earth-stats").text("On Earth, you are" + userAge.earthLife );
+
+        if (userAge.lifeLeft <= 10) {
+          $(".earth-expectancy").text("The galaxy sees that your lifestyle is going to catch up with you. In " + userAge.lifeLeft + " years, you overdose on adderol and Mountain Dew while furiously trying to finish your last Epicodus Code Review. That means you will live to an age of " + userAge.earthExpectancy + " on Earth.");
+        } if else(userAge.lifeLeft >= 11 && userAge.lifeLeft <= 20) {
+          $(".earth-expectancy").text("The galaxy sees your fate. In " + userAge.lifeLeft + " years, you are a succesful software engineer for a hot new start-up, making six figures, living your dream life. However, you fall asleep while smoking a joint after a night out with friends and unintentionally light your penthouse on fire. You die from smoke inhilation , but leave millions behind for your cat. That means, you will live to an age of " + userAge.earthExpectancy + " on Earth.");
+        } if else (userAge.lifeLeft >=21 && userAge.lifeLeft <= 30) {
+          $(".earth-expectancy").text("The galaxy sees your fate. After Epicodus, you succesfully navigated the corporate ladder. You started out as a lowely junior developer, but steadily earned promotion after promotion, until you took over as CTO of a fourtune 500 company in Seattle. However, in " + userAge.lifeLeft + " years, your spouse catches you in bed with the pool boy and kills you both. This means, you will live to an age of " + userAge.earthExpectancy + " on Earth.")
+        } if else (userAge.lifeLeft >=32 && userAge.lifeLeft <= 40) {
+          $(".earth-expectancy").text("The galaxy sees your fate. In " + userAge.lifeLeft + " years, you are living in a trailer in Molalla with your 19 cats. After finishing Epicodus, you decided not to pursue a career in the tech community, and eventually end up selling house plants on Craigslist. One night, one of your cats falls asleep on your face, smothering you to death. That means you will live to an age of " + userAge.earthExpectancy + " years on Earth.");
+        } else {
+          $(".earth-expectancy").text ("The galaxy sees your fate. In " + userAge.lifeLeft + " years, you are staying in your Italian villa. You've just enjoyed a night out with friends where you ate the finest Italian cuisine and drank the finest italian wine. After dinner, you grab some gelato and a bag of coke and decide to lounge in your hot tub. After finishing the gelato and a line of coke, you suddenly have a heart attack and drown in your hot tub. That means you will live to an age of " + userAge.earthExpectancy + " years on Earth".)
+        }
 
     $(".mercury-stats").text("On Mercury, you are" + userAge.mercuryAge + "years old. Based on our galactic calculations, you will live " + userAge.mercuryLife + " years left, which means you will live until you are " + userAge.mercuryExpectancy +".");
 
